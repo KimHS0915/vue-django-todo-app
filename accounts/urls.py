8898681c7052ab259022f4cb4d0e85164a71558d
KimHS0915 <remember2552@gmail.com>
2020-03-10 23:16:22 +0900

    Add register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('/register', views.RegisterCV.as_view(), name="register"),
+    path('/registered', views.RegisteredTV.as_view(), name='registered'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class RegisterCV(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('registered')
+
+
+class RegisteredTV(TemplateView):
+    template_name = 'accounts/registered.html'
