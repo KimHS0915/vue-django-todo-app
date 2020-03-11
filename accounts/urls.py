2ac79061414cfd5a6b783e4e43900130fae90f61
KimHS0915 <remember2552@gmail.com>
2020-03-11 21:19:23 +0900

    Update login, logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from . import views
 
+
 urlpatterns = [
     path('/register', views.RegisterCV.as_view(), name="register"),
     path('/registered', views.RegisteredTV.as_view(), name='registered'),
